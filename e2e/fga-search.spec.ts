bba13ad788e278f351a87928c6902c5c090911b6
Jonas de Abreu Resenes <jabreu.ar@gmail.com>
2025-03-24 12:24:08 -0300

    Update fga-search.spec.ts

--- e2e/fga-search.spec.ts
+++ e2e/fga-search.spec.ts
@@ -8,7 +8,7 @@ test.beforeEach(async ({ page }) => {
 });
 
 test("open clube page", async ({ aiAssert, aiAction }) => {
-  await aiAction('Move your mouse over the forth item in the nav list and click the Clube button');
+  await aiAction('Move the mouse cursor over the forth item in the nav list and click the Clube button');
   // 👀 assert by AI
   await aiAssert("There should be a section titled CLUBES FILIADOS on the page");
 });
